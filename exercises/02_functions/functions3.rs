215f8c17e45d1e0eda7bed7c1cf473efc5e02e6b
Alessio Lombardi <alexlomba87@hotmail.com>
2024-05-07 19:39:38 +0100

    First batch of exercises done

--- exercises/02_functions/functions3.rs
+++ exercises/02_functions/functions3.rs
@@ -3,14 +3,19 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
-    call_me();
+    call_me(None);
+    // call_me();
 }
 
-fn call_me(num: u32) {
-    for i in 0..num {
+// fn call_me() {
+//     println!("Ring!");
+// }
+
+fn call_me(num: Option<u32>) {
+    for i in 0..num.unwrap_or(1) {
         println!("Ring! Call number {}", i + 1);
     }
 }
+
+
